a595d9dddb00b91c2c65e8694c3c655c0521d739
jiweisb23 <JoshWeisberg3@gmail.com>
2021-11-25 11:48:00 -0500

    tinkering with getvehicles html

--- Server.py
+++ Server.py
@@ -7,7 +7,9 @@ import pytz
 import sys
 from powerflowaipulp import *#optimizer
 import jinja2
+import os
 app = Flask(__name__, static_url_path = '/static')
+
 
 #See this to kill: https://stackoverflow.com/questions/4465959/python-errno-98-address-already-in-use
 
